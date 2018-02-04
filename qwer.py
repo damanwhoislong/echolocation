20953d0378da9ef1298b0cdd3a5a97e2d08b4144
Ningsong Shen <ningsongshen123@outlook.com>
2018-02-03 20:17:49 -0500

    clean up code, add commenting

--- qwer.py
+++ qwer.py
@@ -1,26 +1,53 @@
+# Qwer.py
+# Purpose: This file provides the functionality to interact with the Myo and calculate the direction that the user is
+# pointing in. Includes various testing bits. As of 8:11pm Feb. 3, there are two functions
+
+
+# Import the needed items from Myo
 from myo import init, Hub, Feed
-import time
+
+# Advanced mathematical calculations required for quaternion vectors
 import math
 
-
 def get_angle():
+    # Purpose: returns the measurement of yaw from myo. This measurement is used as the direction of movement of
+    # the player in movement.py
+
+    # Why do we need a global variable?
     global myo
+
     if not myo:
+        # When no Myo connects, say so
         print("No Myo connected after 2 seconds")
 
+    # Get the quaternion vectors from the connected Myo
     quat = myo.orientation
-    orientation = {'x': quat.x, 'y': quat.y, 'z': quat.z, 'w': quat.w}
-    #print(orientation)
-    #print(myo.x_direction)
-    roll = math.atan2(2.0*(quat.y*quat.z + quat.w*quat.x), quat.w*quat.w - quat.x*quat.x - quat.y*quat.y + quat.z*quat.z);
-    pitch = math.asin(-2.0 * (quat.x * quat.z - quat.w * quat.y))
+
+    # Testing
+    # print(orientation)
+    # print(myo.x_direction)
+    # orientation = {'x': quat.x, 'y': quat.y, 'z': quat.z, 'w': quat.w}
+
+    # Other irrelevant info
+    # After careful physical testing, it was determined that yaw is what determines the direction
+    # roll = math.atan2(2.0*(quat.y*quat.z + quat.w*quat.x), quat.w*quat.w - quat.x*quat.x - quat.y*quat.y + quat.z*quat.z);
+    # pitch = math.asin(-2.0 * (quat.x * quat.z - quat.w * quat.y))
+
+
+    # Calculate the yaw, and print to console
     yaw = math.atan2(2.0 * (quat.x * quat.y + quat.w * quat.z), quat.w * quat.w + quat.x * quat.x - quat.y * quat.y - quat.z * quat.z)
     print("Yaw:", yaw*180/3.14159265358979323846264338327950288)
-          #"Pitch:", pitch*180/3.14159265358979323846264338327950288, "Roll:", roll*180/3.14159265358979323846264338327950288)
+
+    # Testing for printing the other irrelevant information
+    # "Pitch:", pitch*180/3.14159265358979323846264338327950288, "Roll:", roll*180/3.14159265358979323846264338327950288)
+
+    # Pass the value of yaw to the caller
     return yaw
 
 
 def stahp():
+    # Purpose: closes the connection to the Myo. From the documentation: "Free all resources associated with this Hub.
+    # Will disconnect all connected Myo devices. After shutting down, init must be called before using the Hub again."
     hub.shutdown()  # !! crucial.
 
 
